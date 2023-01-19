b909ef839364d72cf4de540c3803359a17822366
Carlos Ivanchuk <buhodev@proton.me>
2023-01-18 21:00:44 -0400

    build: add @poppanator/sveltekit-svg

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import svg from '@poppanator/sveltekit-svg';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		svg({
+			includePaths: ['./src/lib/icons/'],
+			svgoOptions: {
+				multipass: true,
+				plugins: [
+					{
+						name: 'preset-default',
+						params: { overrides: { removeViewBox: false } }
+					}
+				]
+			}
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
