557aa2a69f18119851d6d6ec6ad5c0f81b5a5cee
Carlos Ivanchuk <buhodev@proton.me>
2023-01-20 22:21:19 -0400

    feat: generate i18n files

---
+++ src/i18n/en/index.ts
@@ -0,0 +1,8 @@
+import type { BaseTranslation } from '../i18n-types';
+
+const en: BaseTranslation = {
+	HI: 'Welcome to SvelteKit',
+	log: `This log was called from '{fileName:string}'`
+};
+
+export default en;
---
+++ src/i18n/es/index.ts
@@ -0,0 +1,8 @@
+import type { Translation } from '../i18n-types';
+
+const es: Translation = {
+	HI: 'Bienvenido a SvelteKit',
+	log: `Este log fue llamado desde '{fileName:string}'`
+};
+
+export default es;
