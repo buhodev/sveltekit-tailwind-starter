4b1ca4c2f931d607f0d82d6445ff5443a9c82310
Carlos Ivanchuk <buhodev@proton.me>
2023-01-20 22:13:35 -0400

    feat: add typesafe-i18n

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,16 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
+
+type Locales = import('$i18n/i18n-types').Locales;
+type TranslationFunctions = import('$i18n/i18n-types').TranslationFunctions;
+
 declare namespace App {
 	// interface Error {}
-	// interface Locals {}
+	interface Locals {
+		locale: Locales;
+		LL: TranslationFunctions;
+	}
 	// interface PageData {}
 	// interface Platform {}
 }
