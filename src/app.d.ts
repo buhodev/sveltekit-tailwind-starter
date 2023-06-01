d088e911739463406244af7fef598deddfc2d852
Carlos Ivanchuk <buhodev@proton.me>
2023-06-01 16:32:35 -0400

    chore: update @poppanator/sveltekit-svg

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,27 +7,3 @@ declare namespace App {
 	// interface PageData {}
 	// interface Platform {}
 }
-
-declare module '*.svg?component' {
-	import type { ComponentType, SvelteComponentTyped } from 'svelte';
-	import type { SVGAttributes } from 'svelte/elements';
-
-	const content: ComponentType<SvelteComponentTyped<SVGAttributes<SVGSVGElement>>>;
-
-	export default content;
-}
-
-declare module '*.svg?src' {
-	const content: string;
-	export default content;
-}
-
-declare module '*.svg?url' {
-	const content: string;
-	export default content;
-}
-
-declare module '*.svg?dataurl' {
-	const content: string;
-	export default content;
-}
